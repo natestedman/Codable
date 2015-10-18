29488f786d5662b93646ee9f55a49fe6a9081784
Nate Stedman <natesm@gmail.com>
2015-10-17 20:06:08 -0400

    Initial commit

---
+++ Encodable/Encodable.h
@@ -0,0 +1,21 @@
+// Encodable
+// Written in 2015 by Nate Stedman <nate@natestedman.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and
+// related and neighboring rights to this software to the public domain worldwide.
+// This software is distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along with
+// this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+@import Foundation;
+
+//! Project version number for Encodable.
+FOUNDATION_EXPORT double EncodableVersionNumber;
+
+//! Project version string for Encodable.
+FOUNDATION_EXPORT const unsigned char EncodableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Encodable/PublicHeader.h>
+
+
